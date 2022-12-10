609dbdf3c503f09985651ba5b16a2993d35125ff
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-12-10 10:10:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Application } from "./components/Application"
+import GlobalStyle from "./styles/global"
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <Application/>
+    </>
+  )
+}
+
+export default App
